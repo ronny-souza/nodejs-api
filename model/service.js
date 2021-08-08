090b548ce4696d48b5e2df29aad9cf10fae4053c
ronny-souza <ronnymarinho2@gmail.com>
2021-08-08 13:23:39 -0300

    Salvando data de agendamento e consulta

--- model/service.js
+++ model/service.js
@@ -1,10 +1,14 @@
-const nodemon = require('nodemon')
+const moment = require('moment')
 const connection = require('../infrastructure/connection')
 
 class Service {
     add(service) {
+        const schedule_date = moment().format('YYYY-MM-DD HH:MM:SS')
+        const service_date = moment(service.service_date, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')
+        const serviceWithDate = {...service, schedule_date, service_date}
         const sql = 'INSERT INTO services SET ?'
-        connection.query(sql, service, (error, result) => {
+
+        connection.query(sql, serviceWithDate, (error, result) => {
             if(error) {
                 console.log(error)
             } else {
